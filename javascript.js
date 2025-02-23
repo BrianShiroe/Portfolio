868daf0a76c82fbcf987316d4a7cfb691662412d
Brian Haw <brianshiroe@gmail.com>
2025-02-24 01:46:13 +0800

    v1

---
+++ javascript.js
@@ -0,0 +1,24 @@
+// Makes the animation trggers when visible, but only once.
+document.addEventListener("DOMContentLoaded", function () {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+          }
+        });
+      },
+      { threshold: 0.35 } // Trigger when 35% of the element is visible
+    );
+  
+    document.querySelectorAll(".animated").forEach((element) => {
+      observer.observe(element);
+    });
+  });
+  
+// image carousel navigation
+function scrollCarousel(direction) {
+  const carousel = document.querySelector(".carousel");
+  const scrollAmount = 319; // Adjust scroll distance
+  carousel.scrollBy({ left: direction * scrollAmount, behavior: "smooth" });
+}
